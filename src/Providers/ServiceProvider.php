5b704b03276c711eb1d102250f215347c0eca679
crazybooot <crazybooot@gmail.com>
2018-05-15 12:14:42 +0300

    initial commit

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,119 @@
+<?php
+declare(strict_types = 1);
+
+namespace Crazybooot\Base64Validation\Providers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use function implode;
+use function trans;
+
+/**
+ * Class QueueStatsServiceProvider
+ *
+ * @package Crazybooot\QueueStats\Providers
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        Validator::extend(
+            'base64max',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Max'
+        );
+
+        Validator::extend(
+            'base64min',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Min'
+        );
+
+        Validator::extend(
+            'base64dimensions',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Dimensions'
+        );
+
+        Validator::extend(
+            'base64file',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64File'
+        );
+
+        Validator::extend(
+            'base64image',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Image'
+        );
+
+        Validator::extend(
+            'base64mimetypes',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Mimetypes'
+        );
+
+        Validator::extend(
+            'base64mimes',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Mimes'
+        );
+
+        Validator::extend(
+            'base64between',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Between'
+        );
+
+        Validator::extend(
+            'base64size',
+            'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Size'
+        );
+
+        Validator::replacer('base64max', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.max.file', ['attribute' => $attribute, 'max' => $parameters[0]]);
+        });
+
+        Validator::replacer('base64min', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.min.file', ['attribute' => $attribute, 'min' => $parameters[0]]);
+        });
+
+        Validator::replacer('base64dimensions', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.dimensions', ['attribute' => $attribute]);
+        });
+
+        Validator::replacer('base64file', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.file', ['attribute' => $attribute]);
+        });
+
+        Validator::replacer('base64image', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.image', ['attribute' => $attribute]);
+        });
+
+        Validator::replacer('base64mimetypes', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.mimetypes', [
+                'attribute' => $attribute,
+                'values'    => implode(',', $parameters)
+            ]);
+        });
+
+        Validator::replacer('base64mimes', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.mimes', [
+                'attribute' => $attribute,
+                'values'    => implode(',', $parameters),
+            ]);
+        });
+
+        Validator::replacer('base64between', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.between.file', [
+                'attribute' => $attribute,
+                'min'       => $parameters[0],
+                'max'       => $parameters[1]
+            ]);
+        });
+
+        Validator::replacer('base64size', function ($message, $attribute, $rule, $parameters) {
+            return trans('validation.size.file', [
+                'attribute' => $attribute,
+                'size'      => $parameters[0],
+            ]);
+        });
+    }
+}
---
+++ src/Validators/Base64Validator.php
@@ -0,0 +1,179 @@
+<?php
+declare(strict_types = 1);
+
+namespace Crazybooot\Base64Validation\Validators;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Concerns\ValidatesAttributes;
+use Symfony\Component\HttpFoundation\File\File;
+use function base64_decode;
+use function explode;
+use function file_put_contents;
+use function strpos;
+use function sys_get_temp_dir;
+use function tempnam;
+use const true;
+
+/**
+ * Class Base64Validator
+ *
+ * @package Crazybooot\Base64Validation\Validators
+ */
+class Base64Validator
+{
+    use ValidatesAttributes;
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Max(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateMax($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Min(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateMin($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Dimensions(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateDimensions($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64File(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateFile($attribute, $this->convertToFile($value))
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Image(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateImage($attribute, $this->convertToFile($value))
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Mimetypes(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateMimetypes($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Mimes(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateMimes($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Between(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateBetween($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string    $attribute
+     * @param mixed     $value
+     * @param array     $parameters
+     * @param Validator $validator
+     *
+     * @return bool
+     */
+    public function validateBase64Size(string $attribute, $value, array $parameters, Validator $validator): bool
+    {
+        return !empty($value)
+            ? $validator->validateSize($attribute, $this->convertToFile($value), $parameters)
+            : true;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return File
+     */
+    protected function convertToFile(string $value): File
+    {
+        if (strpos($value, ';base64') !== false) {
+            [, $value] = explode(';', $value);
+            [, $value] = explode(',', $value);
+        }
+
+        $binaryData = base64_decode($value);
+        $tmpFile = tempnam(sys_get_temp_dir(), 'base64validator');
+        file_put_contents($tmpFile, $binaryData);
+
+        return new File($tmpFile);
+    }
+}
