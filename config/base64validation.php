e7594d6e6eb81f5869c5607084eb6a7a70902f63
crazybooot <crazybooot@gmail.com>
2019-05-14 16:43:05 +0300

    Add ability to create custom rule validation error message localizations

---
+++ config/base64validation.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Replace rule validation message
+    |--------------------------------------------------------------------------
+    |
+    | If set true will be used validation messages for standard
+    | file validation rules (for base64min - validation.min.file message, etc)
+    | for using own messages set this property to false and define message
+    | replacement for each rule you use in afforded by framework way
+    |
+    */
+    'replace_validation_messages' => true,
+];
--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -5,8 +5,11 @@ namespace Crazybooot\Base64Validation\Providers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use function config;
+use function config_path;
 use function implode;
 use function trans;
+use const __DIR__;
 
 /**
  * Class QueueStatsServiceProvider
@@ -67,53 +70,69 @@ class ServiceProvider extends BaseServiceProvider
             'Crazybooot\Base64Validation\Validators\Base64Validator@validateBase64Size'
         );
 
-        Validator::replacer('base64max', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.max.file', ['attribute' => $attribute, 'max' => $parameters[0]]);
-        });
+        if (config('base64validation.replace_validation_messages')) {
+            Validator::replacer('base64max', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.max.file', ['attribute' => $attribute, 'max' => $parameters[0]]);
+            });
 
-        Validator::replacer('base64min', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.min.file', ['attribute' => $attribute, 'min' => $parameters[0]]);
-        });
+            Validator::replacer('base64min', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.min.file', ['attribute' => $attribute, 'min' => $parameters[0]]);
+            });
 
-        Validator::replacer('base64dimensions', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.dimensions', ['attribute' => $attribute]);
-        });
+            Validator::replacer('base64dimensions', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.dimensions', ['attribute' => $attribute]);
+            });
 
-        Validator::replacer('base64file', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.file', ['attribute' => $attribute]);
-        });
+            Validator::replacer('base64file', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.file', ['attribute' => $attribute]);
+            });
 
-        Validator::replacer('base64image', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.image', ['attribute' => $attribute]);
-        });
+            Validator::replacer('base64image', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.image', ['attribute' => $attribute]);
+            });
 
-        Validator::replacer('base64mimetypes', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.mimetypes', [
-                'attribute' => $attribute,
-                'values'    => implode(',', $parameters)
-            ]);
-        });
+            Validator::replacer('base64mimetypes', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.mimetypes', [
+                    'attribute' => $attribute,
+                    'values'    => implode(',', $parameters)
+                ]);
+            });
 
-        Validator::replacer('base64mimes', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.mimes', [
-                'attribute' => $attribute,
-                'values'    => implode(',', $parameters),
-            ]);
-        });
+            Validator::replacer('base64mimes', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.mimes', [
+                    'attribute' => $attribute,
+                    'values'    => implode(',', $parameters),
+                ]);
+            });
 
-        Validator::replacer('base64between', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.between.file', [
-                'attribute' => $attribute,
-                'min'       => $parameters[0],
-                'max'       => $parameters[1]
-            ]);
-        });
+            Validator::replacer('base64between', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.between.file', [
+                    'attribute' => $attribute,
+                    'min'       => $parameters[0],
+                    'max'       => $parameters[1]
+                ]);
+            });
 
-        Validator::replacer('base64size', function ($message, $attribute, $rule, $parameters) {
-            return trans('validation.size.file', [
-                'attribute' => $attribute,
-                'size'      => $parameters[0],
-            ]);
-        });
+            Validator::replacer('base64size', function ($message, $attribute, $rule, $parameters) {
+                return trans('validation.size.file', [
+                    'attribute' => $attribute,
+                    'size'      => $parameters[0],
+                ]);
+            });
+        }
+
+        $this->publishes([
+            __DIR__.'/../../config/base64validation.php' => config_path('base64validation.php'),
+        ], 'config');
+    }
+
+    /**
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/base64validation.php', 'base64validation'
+        );
     }
 }
